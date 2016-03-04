a775fa8f784f1267e0684acf6861e3b63d51adb1
Nectar Shakir <nectar.shakir@gmail.com>
2016-03-04 11:12:02 -0800

    3.4.16
    changes on images and front page

--- front-page.php
+++ front-page.php
@@ -3,20 +3,18 @@
     <h1>WELCOME!</h1>
     <?php bloginfo('description'); ?>
     <h2><span class="underline">Latest News</span></h2>
-    <div class="cta-news">
-      <article class="post-excerpt">
+
+      <article class="front-post-excerpt">
       <?php rewind_posts(); // stop loop one ?>
       <?php query_posts('showposts=6'); // give directions to loop two ?>
       <?php while (have_posts()) : the_post(); // start loop two ?>
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
-        <?php the_post_thumbnail('thumbnail');?></a>
-        <br><?php the_time('F j, Y'); //get the time ?>
-        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail');?>
+        <?php the_title(); ?></a>
+        <h3><br><?php the_time('F j, Y'); //get the time ?></h3>
+        <p><?php the_excerpt(); ?></p>
       </article>
-      <?php endwhile; // end loop two ?>
+    <?php endwhile; // end loop two ?><p>
 <small>front-page.php</small>
     </div>
-  </div>
-
 <?php get_sidebar();?>
 <?php get_footer();?>
